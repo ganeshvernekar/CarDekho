031de853924343b7fa7ce88f697005e8a096b6fb
ganeshvernekar <vernekae031@gmail.com>
2025-02-13 10:28:45 +0530

    firsr-Commit

---
+++ src/Components/shoe/shoe.stories.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import CheckoutPage from "./shoe";
+
+export default{
+    title:"Component/shoe/Checkoutpage",
+    Component:CheckoutPage,
+}
+export const Default=()=><CheckoutPage/>
